62b39e18a12a67ba7fbac1dc57de718b4e7fa1fe
嘿咻傻帽 <xushikuan1@163.com>
2017-02-24 17:50:21 +0800

    Readme

--- src/main/java/com/sillyhat/kafka/one/KafkaConsumerSimpleness.java
+++ src/main/java/com/sillyhat/kafka/one/KafkaConsumerSimpleness.java
@@ -20,23 +20,24 @@ public class KafkaConsumerSimpleness {
     public static void main(String[] args) {
         Properties props = new Properties();
 //./kafka-topics.sh --zookeeper 192.168.233.129:2181,192.168.233.130:2181,192.168.233.131:2181 --list
-        props.put("bootstrap.servers", "192.168.233.129:19001,192.168.233.130:19001,192.168.233.131:19001");
-        props.put("group.id", "groupTestZ");
+        props.put("bootstrap.servers", "192.168.233.129:19001,192.168.233.130:19001,192.168.233.131:19001");// 该地址是集群的子集，用来探测集群。
+        props.put("group.id", "groupTestZ");// cousumer的分组id
 //        props.put("auto.offset.reset", "earliest");//默认latest  是否最大offset,对消费组仅第一次有效
 
-//        props.put("enable.auto.commit", "true");
-//        props.put("auto.commit.interval.ms", "1000");
-//        props.put("session.timeout.ms", "30000");
+//        props.put("enable.auto.commit", "true");// 自动提交offsets
+//        props.put("auto.commit.interval.ms", "1000");// 每隔1s，自动提交offsets
+//        props.put("session.timeout.ms", "30000");// Consumer向集群发送自己的心跳，超时则认为Consumer已经死了，kafka会把它的分区分配给其他进程
         props.put("fetch.min.bytes", 1048576);//1(mb)=1048576字节(b)
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");// 反序列化器
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-        consumer.subscribe(Arrays.asList("test_reset_topic"));
+        consumer.subscribe(Arrays.asList("test_reset_topic"));// 订阅的topic,可以多个
         logger.info("KafkaConsumerExample Begin");
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(30000);
             for (ConsumerRecord<String, String> record : records){
                 logger.info("------------------------------------");
+                logger.info(record.topic());
                 logger.info(record.offset());
                 logger.info(record.key());
                 logger.info(record.value());
--- src/main/java/com/sillyhat/kafka/one/KafkaProducerSimpleness.java
+++ src/main/java/com/sillyhat/kafka/one/KafkaProducerSimpleness.java
@@ -20,17 +20,18 @@ public class KafkaProducerSimpleness {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "192.168.233.129:19001,192.168.233.130:19001,192.168.233.131:19001");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "all");// 记录完整提交，最慢的但是最大可能的持久化
+        props.put("retries", 0);// 请求失败重试的次数
+        props.put("batch.size", 16384);// batch的大小
+        props.put("linger.ms", 1);// 默认情况即使缓冲区有剩余的空间，也会立即发送请求，设置一段时间用来等待从而将缓冲区填的更多，单位为毫秒，producer发送数据会延迟1ms，可以减少发送到kafka服务器的请求数据
+        props.put("buffer.memory", 33554432);// 提供给生产者缓冲内存总量
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");// 序列化的方式，
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         Producer<String, String> producer = new KafkaProducer<String,String>(props);
         for(int i = 0; i < 500000; i++){
             String message = "[" + i + "]";
             logger.info(message);
+            // 三个参数分别为topic, key,value，send()是异步的，添加到缓冲区立即返回，更高效。
             producer.send(new ProducerRecord<String,String>("test_reset_topic", "luckKey", message));
             try {
                 Thread.sleep(1000);
